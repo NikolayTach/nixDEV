13ac913078818627dc5ff5ec9c3cc78ac68dd658
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2022-04-25 21:00:36 +0300

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -2,9 +2,9 @@
 
 let
   mach-nix = import (builtins.fetchGit {
-    url = “https://github.com/DavHau/mach-nix”;
-url = “https://github.com/kohsuke/com4j”;
-    ref = “refs/tags/3.3.0”;
+    url = "https://github.com/DavHau/mach-nix";
+  url = "https://github.com/kohsuke/com4j";
+    ref = "refs/tags/3.3.0";
   }) { };
 
 in pkgs.mkShell {
