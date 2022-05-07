35ea17d68455bad060f1c223d71d93c6110b2251
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2022-05-07 12:28:57 +0300

    Update Liquid_gel.nix

--- Liquid_gel.nix
+++ Liquid_gel.nix
@@ -2,7 +2,7 @@
 {
 	{
 	$CONFIG_SET
-read_value_frame:("log_set1","log_set2","log_set3");
+read_value_frame:("log_set3");
 nix-store 
 ref = "os/read/file/nix-store/5870oR512X/lork.ddl";
 set value(0);
