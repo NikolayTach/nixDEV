1fe70e1cbe15fd0c9bff47f01ae8c06482491dfe
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2022-05-07 12:41:37 +0300

    Update Liquid_gel.nix

--- Liquid_gel.nix
+++ Liquid_gel.nix
@@ -1,17 +1,6 @@
 {nix,config,pkgs,...}:
 {
- 	ALL
-	to 
-	$LIQUID_
-	GEL34_LORW_SET_34.TREE
-	set
-	ALL
-	to
-	$CONFIG
-	set
-	to
-	ALL
-	$SET
+
 ref = "os/read/file/nix-store/5870oR512X/lork.ddl";
 set value 
 	null
