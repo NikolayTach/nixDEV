0432b167b6cdc92c2239d5452d1eec3619752e5d
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2022-05-07 12:44:15 +0300

    Update Liquid_gel.nix

--- Liquid_gel.nix
+++ Liquid_gel.nix
@@ -10,9 +10,9 @@ set value
     home-manager.url = "github:nix-community/home-manager";
   };
 }
-$CONFIG_SET
-"[/,g@st ]"
-read_frame:"log_set1","log_set2","log_set3"
-nix-store -A "os/read/file/nix-store/5870oR512X/lork.ddl";
+	$CONFIG_SET to 
+	"[/,g@st ]"
+read_frame:["log_set1"],["log_set2"],["log_set3"]
+nix-store  "os/read/file/nix-store/5870oR512X/lork.ddl";
 set value(0);
 
