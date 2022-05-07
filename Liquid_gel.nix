b11a5bce1672395dd1f4ef7bf3ff44b950cdbf26
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2022-05-07 12:14:31 +0300

    Add files via upload

---
+++ Liquid_gel.nix
@@ -0,0 +1,23 @@
+{nix,config,pkgs,...}:
+{
+$CONFIG_SET
+"[/,g@st ]"
+read_frame:"log_set1","log_set2","log_set3"
+nix-store -A "os/read/file/nix-store/5870oR512X/lork.ddl";
+set value(0);
+	{
+[dev_synch for /drive344312@34];
+read for null 
+nogue/oji/jvm-mng/PACKS139
+\set as 
+whole or null function
+		read 
+		$TREE%SET&EDGE;
+win set null};
+	{
+  inputs = {
+    home-manager.url = "github:nix-community/home-manager";
+  };
+}
+
+}
