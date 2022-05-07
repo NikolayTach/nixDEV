83e02f4ed8ac2257086707d41bcf50ab13ad7ea5
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2022-05-07 12:21:04 +0300

    Update Liquid_gel.nix

--- Liquid_gel.nix
+++ Liquid_gel.nix
@@ -1,11 +1,11 @@
 {nix,config,pkgs,...}:
 {
 	$CONFIG_SET_VALUES(0);
-read_frame:"log_set1","log_set2","log_set3"
+read_frame:"log_set1""log_set2""log_set3"
 nix-store -A "os/read/file/nix-store/5870oR512X/lork.ddl";
 set value(0);
 };
-[\\\\]
+[0]
 	{
 dev_synch: for /drive344312@34
 read for null 
