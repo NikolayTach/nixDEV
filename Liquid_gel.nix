e885ec941882a297094f4fab3316f170d1f68eea
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2022-05-07 12:48:50 +0300

    Update Liquid_gel.nix

--- Liquid_gel.nix
+++ Liquid_gel.nix
@@ -11,8 +11,8 @@ set value
   };
 }
 	{
-	$SET_VALUE_TREADER_NIX_BIN		
-	"[/,g@st ]"
+	mkdir -A nix-bin/reader-module1 $SET_VALUE_TREADER_NIX_BIN		
+	
 read_frame:["log_set1"],["log_set2"],["log_set3"]
 nix-store  "os/read/file/nix-store/5870oR512X/lork.ddl"
 set value(0)
