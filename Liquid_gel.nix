291c8d1758df70b694cbe907b10db3178c68698e
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2022-05-07 12:39:40 +0300

    Update Liquid_gel.nix

--- Liquid_gel.nix
+++ Liquid_gel.nix
@@ -1,12 +1,14 @@
 {nix,config,pkgs,...}:
 {
  $LIQUID_GEL34_LORW_SET_34.TREE
- $CONFIG
- $SET
+	set
+	$CONFIG
+	set
+	$SET
 ref = "os/read/file/nix-store/5870oR512X/lork.ddl";
 set value 
 	null
-		{
+		
   inputs = {
     home-manager.url = "github:nix-community/home-manager";
   };
