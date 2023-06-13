46b8272acd1716cb55629fb5377ca9a600f60dc6
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2023-06-13 12:53:25 -0700

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,14 +1,18 @@
-{ pkgs,...}:
+{ pkgs, ... }:
 
 let
   mach-nix = import (builtins.fetchGit {
     url = "https://github.com/DavHau/mach-nix";
-  url = "https://github.com/kohsuke/com4j";
     ref = "refs/tags/3.3.0";
-  }) { };
+  }) {};
+
+  com4j = import (builtins.fetchGit {
+    url = "https://github.com/kohsuke/com4j";
+    ref = "refs/tags/3.3.0";
+  }) {};
 
 in pkgs.mkShell {
   buildInputs = [ (mach-nix.mkPython {
-    packagesExtra = [ etc/rev/nix/349348fd1958398bdfed1421255324512425/ ];
+    packagesExtra = [ pkgs.etc pkgs.rev pkgs.nix ];
   }) ];
 }
